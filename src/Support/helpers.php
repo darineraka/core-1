8ec2bbed3c34dffb5b16c5074ef9945ee8fff82b
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-03-21 22:41:00 -0500

    Comments on helpers

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -2,6 +2,11 @@
 
 if (!function_exists('core_templates_path'))
 {
+    /**
+     * Gets the core templates path.
+     *
+     * @return string
+     */
     function core_templates_path()
     {
         return base_path().'/templates/system/core/';
@@ -10,6 +15,12 @@ if (!function_exists('core_templates_path'))
 
 if (!function_exists('load_system_template'))
 {
+    /**
+     * Get the contents of a system template by name.
+     *
+     * @param $templateName
+     * @return null|string
+     */
     function load_system_template($templateName)
     {
         $templateFile = base_path().'/templates/system/'.$templateName.'.newup';
