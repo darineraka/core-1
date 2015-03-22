6c1a0a4caef6bd3d61e01f83e3d902513d89b5fd
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-03-21 20:58:17 -0500

    Custom application

---
+++ src/Foundation/Application.php
@@ -0,0 +1,14 @@
+<?php namespace NewUp\Foundation;
+
+use Illuminate\Foundation\Application as LaravelApplication;
+
+class Application extends LaravelApplication {
+
+    /**
+     * The NewUp utility version.
+     *
+     * @var string
+     */
+    const VERSION = '1.0.0';
+
+}
