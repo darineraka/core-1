93322e7237e8afae7d98bdf9d255da2822b25e57
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-03-27 15:09:58 -0500

    NewUp entry point

---
+++ newup.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+<?php
+
+
+require __DIR__ . '/bootstrap/autoload.php';
+
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+$kernel = $app->make('Illuminate\Contracts\Console\Kernel');
+$kernel->bootstrap();
+
+$status = $kernel->handle(
+    $input = new Symfony\Component\Console\Input\ArgvInput,
+    new Symfony\Component\Console\Output\ConsoleOutput
+);
+
+$kernel->terminate($input, $status);
+
+exit($status);
