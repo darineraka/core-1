fcf933131213e771e2dbd621cc7709702c3f4e9a
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-03-27 15:11:39 -0500

    Removed incomplete commands from the kernel

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -6,14 +6,6 @@ class Kernel extends BaseKernel {
 
     protected $commands = [
         'NewUp\Console\Commands\About',
-        'NewUp\Console\Commands\Init',
-        'NewUp\Console\Commands\Builder',
-        'NewUp\Console\Commands\ListTemplates',
-        'NewUp\Console\Commands\UpdateSystem',
-        'NewUp\Console\Commands\UpdateTemplate',
-        'NewUp\Console\Commands\RemoveTemplate',
-        'NewUp\Console\Commands\CheckTemplate',
-        'NewUp\Console\Commands\InstallTemplate',
     ];
 
 
