8a6b47f7f2151121fbcfa37b0c01c1b31250761f
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-04-19 15:18:18 -0500

    Added file system generator

---
+++ src/Templates/Generators/FileSystemTreeGenerator.php
@@ -0,0 +1,113 @@
+<?php namespace NewUp\Templates\Generators;
+
+use Illuminate\Filesystem\Filesystem;
+use NewUp\Contracts\IO\FileTreeGenerator;
+
+class FileSystemTreeGenerator implements FileTreeGenerator {
+
+    /**
+     * A collection of paths and directories to create.
+     *
+     * @var array
+     */
+    protected $paths = [];
+
+    /**
+     * The file system instance.
+     *
+     * @var Filesystem
+     */
+    protected $fileSystem;
+
+    public function __construct(Filesystem $fileSystem)
+    {
+        $this->fileSystem = $fileSystem;
+    }
+
+    /**
+     * Adds paths to the file tree generator.
+     *
+     * The paths added using this method should already be processed, meaning that
+     * the final path is already free of template processors, placeholders, etc.
+     *
+     * @param $paths
+     * @return mixed
+     */
+    public function addPaths($paths)
+    {
+        $this->paths = $this->paths + $paths;
+    }
+
+    /**
+     * Converts all instances of '\' or '/' to the system directory separator.
+     */
+    private function normalizePaths()
+    {
+        foreach ($this->paths as $key => $path)
+        {
+
+            $newPath = str_replace('/', DIRECTORY_SEPARATOR, $path);
+            $newPath = str_replace('\\', DIRECTORY_SEPARATOR, $newPath);
+
+            $this->paths[$key] = $newPath;
+        }
+    }
+
+    /**
+     * Sorts the paths so that the most specific paths are listed first.
+     *
+     * @return array
+     */
+    private function sortPaths()
+    {
+        $this->normalizePaths();
+
+        uasort($this->paths, function($a, $b)
+        {
+            if ($a == $b)
+            {
+                return 0;
+            }
+
+            return (substr_count($a['path'], DIRECTORY_SEPARATOR) > substr_count($b['path'], DIRECTORY_SEPARATOR)) ? -1 : 1;
+        });
+
+        return $this->paths;
+    }
+
+    /**
+     * Gets the paths that will be created.
+     *
+     * @return mixed
+     */
+    public function getPaths()
+    {
+        return $this->sortPaths();
+    }
+
+    /**
+     * Creates the file tree in the given directory.
+     *
+     * @param $destinationDirectory
+     * @return mixed
+     */
+    public function generate($destinationDirectory)
+    {
+        foreach ($this->getPaths() as $path)
+        {
+            $fullPath = $destinationDirectory.DIRECTORY_SEPARATOR.$path['path'];
+            $mode     = 0755;
+
+            if ($path['type'] == 'dir')
+            {
+                $this->fileSystem->makeDirectory($fullPath, $mode, true, true);
+            }
+            else
+            {
+                $this->fileSystem->put($fullPath, '');
+            }
+        }
+    }
+
+
+}
---
+++ tests/Generators/FileSystemTreeGeneratorTest.php
@@ -0,0 +1,36 @@
+<?php namespace NewUp\Tests\Generators;
+
+use NewUp\Templates\Generators\FileSystemTreeGenerator;
+
+class FileSystemTreeGeneratorTest extends \PHPUnit_Framework_TestCase {
+
+    private function getGenerator()
+    {
+        $fileSystem = $this->getMock('Illuminate\Filesystem\Filesystem');
+        $generator = new FileSystemTreeGenerator($fileSystem);
+        return $generator;
+    }
+
+    public function testAddPathsWorks()
+    {
+        $g = $this->getGenerator();
+        $g->addPaths(['key' => ['path' => '', 'type' => '']]);
+        $this->assertCount(1, $g->getPaths());
+    }
+
+    public function testGeneratorIsSortingThePathsInTheCorrectOrder()
+    {
+        $g = $this->getGenerator();
+
+        $startPaths = [
+          'first.php' => ['path' => 'this/should/appear/last/file.php', 'type' => 'file'],
+          'second' => ['path' => 'this/should/appear/first/because/its/nested/further/file.php', 'type' => 'file']
+        ];
+
+        $g->addPaths($startPaths);
+
+        // Just test the keys, as the directory separators are normalized.
+        $this->assertEquals(['second', 'first.php'], array_keys($g->getPaths()));
+
+    }
+}
