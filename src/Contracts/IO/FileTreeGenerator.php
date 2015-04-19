e7adc20f03675ae7177d08eac4c664d561b9a767
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-04-19 16:11:45 -0500

    Missing contract

---
+++ src/Contracts/IO/FileTreeGenerator.php
@@ -0,0 +1,27 @@
+<?php namespace NewUp\Contracts\IO;
+
+interface FileTreeGenerator {
+
+    /**
+     * Adds paths to the file tree generator.
+     *
+     * @param $paths
+     */
+    public function addPaths($paths);
+
+    /**
+     * Gets the paths that will be created.
+     *
+     * @return array
+     */
+    public function getPaths();
+
+    /**
+     * Creates the file tree in the given directory.
+     *
+     * @param $destinationDirectory
+     * @return void
+     */
+    public function generate($destinationDirectory);
+
+}
