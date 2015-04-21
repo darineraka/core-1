72951cae7cbacca934f6b60a15ea324d9522c7fe
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-04-21 16:26:43 -0500

    PSR-2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,11 @@
 <?php
 
-$app = new NewUp\Foundation\Application(realpath(__DIR__.'/../'));
+$app = new NewUp\Foundation\Application(realpath(__DIR__ . '/../'));
 $app->useStoragePathForOptimizations();
-$app->useStoragePath(realpath(__DIR__.'/../storage/'));
+$app->useStoragePath(realpath(__DIR__ . '/../storage/'));
 
 $app->singleton(
-'Illuminate\Contracts\Console\Kernel',
+    'Illuminate\Contracts\Console\Kernel',
     'NewUp\Console\Kernel'
 );
 
